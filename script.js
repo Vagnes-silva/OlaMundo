b3043dc2c16f83f78fc2b2889d7098003c0607c3
Vagnes-silva <100873980+Vagnes-silva@users.noreply.github.com>
2025-07-31 15:00:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function inserirNome(){
-    let nomeUsuario = prompt("Qual é o seu nome?")
-    let elemento = document.querySelector('#nome-cursista')
-    elemento.textContent = nomeUsuario;
+// function inserirNome(){
+//     let nomeUsuario = prompt("Qual é o seu nome?")
+//     let elemento = document.querySelector('#nome-cursista')
+//     elemento.textContent = nomeUsuario;
+// }
+
+let aluno1 = {
+    nome: "Vagnes",
+    idade: 52,
+    anoLetivo: "3° série",
+    materiaFavorita: ["Matemátiva", "Física", "Programação", "Robotica"]
 }
+console.log(aluno1.materiaFavorita[2])
+
+const objeto = document.querySelector("#alunos");
+objeto.textContent = aluno1.nome;
+//objeto.textContent = aluno1.idade;
